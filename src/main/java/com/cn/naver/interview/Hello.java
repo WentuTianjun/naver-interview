e60d4f1574f58a16a3b142ed0f63c7e1c4c698f6
liuchen <liuchen25@gome.com.cn>
2020-04-11 10:13:02 +0800

    project build

---
+++ src/main/java/com/cn/naver/interview/Hello.java
@@ -0,0 +1,12 @@
+package com.cn.naver.interview;
+
+/**
+ * @author liuchen25
+ * @description
+ * @time 2020/4/11 9:58
+ */
+public class Hello {
+    public static void print() {
+        System.out.println("Hello Word!");
+    }
+}
---
+++ src/test/java/com/cn/naver/interview/HelloTest.java
@@ -0,0 +1,16 @@
+package com.cn.naver.interview;
+
+import org.junit.Test;
+
+/**
+ * @author liuchen25
+ * @description
+ * @time 2020/4/11 9:58
+ */
+public class HelloTest {
+
+    @Test
+    public void testPrint(){
+        Hello.print();
+    }
+}
